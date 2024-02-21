0e9a6e212cf9bc011b3999aed407a3105f5f88f1
Siarhei Alakayeu <Siarhei.Alakayeu@NEOVAC.CH>
2024-02-21 11:17:05 +0300

    201. Bitwise AND of Numbers Range

---
+++ LeetCode/Questions/Bitwise AND of Numbers Range 201.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode.Questions;
+
+public static class Bitwise_AND_of_Numbers_Range_201
+{
+    public static int RangeBitwiseAnd(int left, int right)
+    {
+        while (right > left)
+        {
+            right &= right - 1;
+        }
+
+        return right;
+    }
+}
