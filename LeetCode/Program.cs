b99a6a8a9ce4ad3e9961c3d2cbc97c0d25196d5b
Siarhei Alakayeu <Siarhei.Alakayeu@NEOVAC.CH>
2024-02-22 10:05:37 +0300

    997. Find the Town Judge

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -10,8 +10,9 @@ namespace LeetCode
             // Leaf_Similar_Trees_872.LeafSimilar(Leaf_Similar_Trees_872.GetRoot1(), Leaf_Similar_Trees_872.GetRoot2());
             // Find_First_Palindromic_String_in_the_Array_2108.FirstPalindrome(["abc", "car", "ada", "racecar", "cool"]);
             // Rearrange_Array_Elements_by_Sign_2149.RearrangeArray([3, 1, -2, -5, 2, -4]);
-            //var result = Find_Polygon_With_the_Largest_Perimeter_2971.LargestPerimeter([1,12,1,2,5,50,3]);
-            Bitwise_AND_of_Numbers_Range_201.RangeBitwiseAnd(0, 0);
+            // var result = Find_Polygon_With_the_Largest_Perimeter_2971.LargestPerimeter([1,12,1,2,5,50,3]);
+            // Bitwise_AND_of_Numbers_Range_201.RangeBitwiseAnd(0, 0);
+            Find_the_Town_Judge_997.FindJudge(2, [[1,2]]);
         }
     }
 }
---
+++ LeetCode/Questions/Find the Town Judge 997.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Questions;
+
+public static class Find_the_Town_Judge_997
+{
+    public static int FindJudge(int n, int[][] trust)
+    {
+        var howMuchTrustAPerson = new int[n + 1];
+        var whoDoesThePersonTrust = new int[n + 1];
+
+        foreach (var t in trust)
+        {
+            howMuchTrustAPerson[t[1]]++;
+            whoDoesThePersonTrust[t[0]]++;
+        }
+
+        for (var i = 1; i <= n; i++)
+        {
+            if (howMuchTrustAPerson[i] == n - 1 && whoDoesThePersonTrust[i] == 0)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
