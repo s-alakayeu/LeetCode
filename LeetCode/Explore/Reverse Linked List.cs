cff3027ebc97251c3e0d99315a6ec23ca6962009
s-alakayeu <allakaevs@gmail.com>
2024-03-06 12:37:13 +0300

    Add Explore/ReverseLinkedList

---
+++ LeetCode/Explore/Reverse Linked List.cs
@@ -0,0 +1,26 @@
+﻿using LeetCode.Structures;
+
+namespace LeetCode.Explore;
+
+public static class Reverse_Linked_List
+{
+    public static ListNode PrepareData()
+    {
+        return new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+    }
+    
+    public static ListNode ReverseList(ListNode head) {
+        ListNode previous = null;
+        
+        while (head is not null)
+        {
+            var temp = head?.next;
+            head.next = previous;
+            previous = head;
+            head = temp;
+            
+        }
+        
+        return previous;
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using LeetCode.Explore;
 using LeetCode.Questions;
 
 namespace LeetCode
@@ -12,7 +13,8 @@ namespace LeetCode
             // Rearrange_Array_Elements_by_Sign_2149.RearrangeArray([3, 1, -2, -5, 2, -4]);
             // var result = Find_Polygon_With_the_Largest_Perimeter_2971.LargestPerimeter([1,12,1,2,5,50,3]);
             // Bitwise_AND_of_Numbers_Range_201.RangeBitwiseAnd(0, 0);
-            Find_the_Town_Judge_997.FindJudge(2, [[1,2]]);
+            // Find_the_Town_Judge_997.FindJudge(2, [[1,2]]);
+            Reverse_Linked_List.ReverseList(Reverse_Linked_List.PrepareData());
         }
     }
 }
