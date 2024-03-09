d9db208864a4c125401518a506d9f052f48b3f7b
Siarhei Alakayeu <allakaevs@gmail.com>
2024-03-09 18:41:49 +0300

    2540. Minimum Common Value

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -18,7 +18,8 @@ namespace LeetCode
             //Remove_Linked_List_Elements.RemoveElements(Remove_Linked_List_Elements.PrepareData(), 6);
             //Odd_Even_Linked_List.OddEvenList(Odd_Even_Linked_List.PrepareData());
             //Palindrome_Linked_List.IsPalindrome(Palindrome_Linked_List.PrepareData());
-            Count_Elements_With_Maximum_Frequency_3005.MaxFrequencyElements([1, 2, 2, 3, 1, 4]);
+            //Count_Elements_With_Maximum_Frequency_3005.MaxFrequencyElements([1, 2, 2, 3, 1, 4]);
+            Minimum_Common_Value_2540.GetCommon([6,13,18,18,28,34,37,39,46,50,52,54,62,63,65,66,75,80,97,98], [10,13,13,19,27,33,40,41,43,46,56,61,69,72,78,79,82,88,91,94]);
         }
     }
 }
---
+++ LeetCode/Questions/Minimum Common Value 2540.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Questions;
+
+public static class Minimum_Common_Value_2540
+{
+    public static int GetCommon(int[] nums1, int[] nums2)
+    {
+        var hashSet = new HashSet<int>(nums1);
+        
+        foreach (var num in nums2)
+        {
+            if (hashSet.Contains(num))
+            {
+                return num;
+            }
+        }
+
+        return -1;
+    }
+}
