5671aa9d94d852e7aaf6e7e42483347604bff430
s-alakayeu <allakaevs@gmail.com>
2024-03-11 10:43:23 +0300

    791. Custom Sort String

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -15,12 +15,11 @@ namespace LeetCode
             // Bitwise_AND_of_Numbers_Range_201.RangeBitwiseAnd(0, 0);
             // Find_the_Town_Judge_997.FindJudge(2, [[1,2]]);
             // Reverse_Linked_List.ReverseList(Reverse_Linked_List.PrepareData());
-            //Remove_Linked_List_Elements.RemoveElements(Remove_Linked_List_Elements.PrepareData(), 6);
-            //Odd_Even_Linked_List.OddEvenList(Odd_Even_Linked_List.PrepareData());
-            //Palindrome_Linked_List.IsPalindrome(Palindrome_Linked_List.PrepareData());
-            //Count_Elements_With_Maximum_Frequency_3005.MaxFrequencyElements([1, 2, 2, 3, 1, 4]);
-            //Minimum_Common_Value_2540.GetCommon([6,13,18,18,28,34,37,39,46,50,52,54,62,63,65,66,75,80,97,98], [10,13,13,19,27,33,40,41,43,46,56,61,69,72,78,79,82,88,91,94]);
-            Intersection_of_Two_Arrays_349.Intersection([1, 2, 2, 1], [2, 2]);
+            // Remove_Linked_List_Elements.RemoveElements(Remove_Linked_List_Elements.PrepareData(), 6);
+            // Odd_Even_Linked_List.OddEvenList(Odd_Even_Linked_List.PrepareData());
+            // Palindrome_Linked_List.IsPalindrome(Palindrome_Linked_List.PrepareData());
+            // Intersection_of_Two_Arrays_349.Intersection([1, 2, 2, 1], [2, 2]);
+            Custom_Sort_String_791.CustomSortString("cba", "abcd");
         }
     }
 }
---
+++ LeetCode/Questions/Custom Sort String 791.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Questions;
+
+public static class Custom_Sort_String_791
+{
+    public static string CustomSortString(string order, string s)
+    {
+        var sortOrder = new int[26];
+
+        for (var i = 0; i < order.Length; i++)
+        {
+            sortOrder[order[i] - 'a'] = i + 1;
+        }
+
+        var str = s.ToCharArray();
+
+        Array.Sort(str, (a, b) => sortOrder[a - 'a'] - sortOrder[b - 'a']);
+
+        return new string(str);
+    }
+}
