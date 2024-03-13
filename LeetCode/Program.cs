5f06e2e6d40c035b3a0c4c56f41968306b0eedd3
s-alakayeu <allakaevs@gmail.com>
2024-03-13 11:36:32 +0300

    2485. Find the Pivot Integer

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -22,7 +22,8 @@ namespace LeetCode
             // Custom_Sort_String_791.CustomSortString("cba", "abcd");
             // Merge_Two_Sorted_Lists_21.MergeTwoLists(Merge_Two_Sorted_Lists_21.PrepareData1(), Merge_Two_Sorted_Lists_21.PrepareData2());
             // Add_Two_Numbers_2.AddTwoNumbers(Add_Two_Numbers_2.PrepareDataFirst(), Add_Two_Numbers_2.PrepareDataSecond());
-            Remove_Zero_Sum_Consecutive_Nodes_from_Linked_List_1171.RemoveZeroSumSublists(Remove_Zero_Sum_Consecutive_Nodes_from_Linked_List_1171.PrepareData());
+            //Remove_Zero_Sum_Consecutive_Nodes_from_Linked_List_1171.RemoveZeroSumSublists(Remove_Zero_Sum_Consecutive_Nodes_from_Linked_List_1171.PrepareData());
+            Find_the_Pivot_Integer_2485.PivotInteger(8);
         }
     }
 }
---
+++ LeetCode/Questions/Find the Pivot Integer 2485.cs
@@ -0,0 +1,15 @@
+﻿namespace LeetCode.Questions;
+
+public static class Find_the_Pivot_Integer_2485
+{
+    public static int PivotInteger(int n)
+    {
+        var pivot = Math.Sqrt(n * (n + 1) / (double)2);
+        if (pivot - Math.Floor(pivot) == 0)
+        {
+            return (int)pivot;   
+        }
+        
+        return -1;
+    }
+}
