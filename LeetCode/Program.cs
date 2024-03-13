194d96094cc7b9f4f7645ef4ac2719b8b726f9b3
s-alakayeu <allakaevs@gmail.com>
2024-03-13 15:53:29 +0300

    147. Insertion Sort List

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -24,7 +24,8 @@ namespace LeetCode
             // Add_Two_Numbers_2.AddTwoNumbers(Add_Two_Numbers_2.PrepareDataFirst(), Add_Two_Numbers_2.PrepareDataSecond());
             //Remove_Zero_Sum_Consecutive_Nodes_from_Linked_List_1171.RemoveZeroSumSublists(Remove_Zero_Sum_Consecutive_Nodes_from_Linked_List_1171.PrepareData());
             // Find_the_Pivot_Integer_2485.PivotInteger(8);
-            Height_Checker_1051.HeightChecker([1, 1, 4, 2, 1, 3]);
+            // Height_Checker_1051.HeightChecker([1, 1, 4, 2, 1, 3]);
+            Insertion_Sort_List_147.InsertionSortList(Insertion_Sort_List_147.PrepareData());
         }
     }
 }
---
+++ LeetCode/Questions/Insertion Sort List 147.cs
@@ -0,0 +1,50 @@
+﻿using LeetCode.Structures;
+
+namespace LeetCode.Questions;
+
+public static class Insertion_Sort_List_147
+{
+    public static ListNode PrepareData()
+    {
+        return new ListNode(4, new ListNode(2, new ListNode(1, new ListNode(3))));
+    }
+    
+    public static ListNode InsertionSortList(ListNode head)
+    {
+        var output = null as ListNode;
+
+        foreach (var input in Enumerate(head))
+        {
+            ref var node = ref FindInsertion(ref output, input);
+
+            // insert
+            input.next = node;
+            node = input;
+        }
+
+        return output;
+    }
+
+    private static IEnumerable<ListNode> Enumerate(ListNode node)
+    {
+        while (node is not null)
+        {
+            var next = node.next;
+            yield return node;
+            node = next;
+        }
+    }
+
+    private static ref ListNode FindInsertion(ref ListNode output, ListNode input)
+    {
+        ref var node = ref output;
+
+        while (node != null && node.val < input.val)
+        {
+            node = ref node.next;
+        }
+
+        return ref node;
+    }
+}
+
