ed06bb937489a415a670eebcbad9229d378086a6
s-alakayeu <allakaevs@gmail.com>
2024-03-27 11:55:19 +0300

    713. Subarray Product Less Than K

---
+++ LeetCode/Questions/Subarray Product Less Than K 713.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCode.Questions;
+
+public static class Subarray_Product_Less_Than_K_713
+{
+    public static int NumSubarrayProductLessThanK(int[] nums, int k) {
+        if (k <= 1)
+        {
+            return 0;
+        }
+        
+        var count = 0;
+        var product = 1;
+        var left = 0;
+        
+        for (var right = 0; right < nums.Length; right++) {
+            product *= nums[right];
+            
+            while (product >= k && left <= right) {
+                product /= nums[left];
+                left++;
+            }
+            
+            count += right - left + 1;
+        }
+        
+        return count;
+    }
+}
