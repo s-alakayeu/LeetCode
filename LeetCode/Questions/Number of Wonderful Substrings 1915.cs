e517a2672cf0d9e6c3e49b23123bad73b8d49f19
s-alakayeu <allakaevs@gmail.com>
2024-04-30 10:41:31 +0300

    1915. Number of Wonderful Substrings

---
+++ LeetCode/Questions/Number of Wonderful Substrings 1915.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Questions;
+
+public static class Number_of_Wonderful_Substrings_1915
+{
+    public static long WonderfulSubstrings(string word) {
+        long result = 0;
+        var count = new int[1024];
+        count[0] = 1;
+        var mask = 0;
+
+        foreach (var c in word) {
+            mask ^= 1 << (c - 'a');
+            result += count[mask];
+            for (var i = 0; i < 10; i++) {
+                result += count[mask ^ (1 << i)];
+            }
+            count[mask]++;
+        }
+
+        return result;
+    }
+}
