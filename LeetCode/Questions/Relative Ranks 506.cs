3a0f8ec2ddb70308410d97b102662f5dac863020
s-alakayeu <allakaevs@gmail.com>
2024-05-08 09:46:54 +0300

    506. Relative Ranks

---
+++ LeetCode/Questions/Relative Ranks 506.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCode.Questions;
+
+public static class Relative_Ranks_506
+{
+    public static string[] FindRelativeRanks(int[] score) {
+        const string firstPlace = "Gold Medal";
+        const string secondPlace = "Silver Medal";
+        const string thirdPlace = "Bronze Medal";
+
+        var stringScore = new string[score.Length];
+        
+        var orderedScore = score.OrderDescending().ToArray();
+
+        for (var i = 0; i < score.Length; i++)
+        {
+            for (var j = 0; j < score.Length; j++)
+            {
+                if (score[i] != orderedScore[j])
+                {
+                    continue;
+                }
+
+                stringScore[i] = j switch
+                {
+                    0 => firstPlace,
+                    1 => secondPlace,
+                    2 => thirdPlace,
+                    _ => (j + 1).ToString()
+                };
+
+                break;
+            }
+        }
+
+        return stringScore;
+    }
+}
