eceff6a69ee10cd591601d279c1f766c5ea232fa
s-alakayeu <allakaevs@gmail.com>
2024-05-20 09:59:07 +0300

    1863. Sum of All Subset XOR Totals

---
+++ LeetCode/Questions/Sum of All Subset XOR Totals 1863.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Questions;
+
+public static class Sum_of_All_Subset_XOR_Totals_1863
+{
+    public static int SubsetXORSum(int[] nums) 
+    {
+        return Helper(nums, 0, 0);
+    }
+
+    private static int Helper(int[] nums, int index, int currValue) 
+    {
+        if (index == nums.Length)
+        {
+            return currValue;
+        }
+        
+        return Helper(nums, index + 1, currValue ^ nums[index]) + Helper(nums, index + 1, currValue);
+    }
+}
