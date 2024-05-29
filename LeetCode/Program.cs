d79098955ed3fd8c3b7f10bdd376686df94a669c
s-alakayeu <allakaevs@gmail.com>
2024-05-29 10:31:51 +0300

    1404. Number of Steps to Reduce a Number in Binary Representation to One

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -63,7 +63,8 @@ namespace LeetCode
             // Subsets_78.Subsets([1, 2, 3]);
             // The_Number_of_Beautiful_Subsets_2597.BeautifulSubsets([1,1,2,3], 1);
             // Special_Array_With_X_Elements_Greater_Than_or_Equal_X_1608.SpecialArray([0, 4, 3, 0, 4]);
-            Get_Equal_Substrings_Within_Budget_1208.EqualSubstring("abcd", "bcdf", 3);
+            // Get_Equal_Substrings_Within_Budget_1208.EqualSubstring("abcd", "bcdf", 3);
+            Number_of_Steps_to_Reduce_a_Number_in_Binary_Representation_to_One_1404.NumSteps("1101");
         }
     }
 }
---
+++ LeetCode/Questions/Number of Steps to Reduce a Number in Binary Representation to One 1404.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+namespace LeetCode.Questions;
+
+public static class Number_of_Steps_to_Reduce_a_Number_in_Binary_Representation_to_One_1404
+{
+    public static int NumSteps(string s)
+    {
+        var builder = new StringBuilder(s);
+        var count = 0;
+        
+        while (builder.Length != 1)
+        {
+            count++;
+            
+            if (builder[^1] == '1')
+            {
+                var index = builder.Length - 1;
+                
+                builder[index] = '0';
+
+                while (true)
+                {
+                    index--;
+
+                    if (index < 0)
+                    {
+                        builder.Append("0", 0, 1);
+                        index++;
+                    }
+                    if (builder[index] > '0')
+                    {
+                        builder[index] = '0';
+                    }
+                    else
+                    {
+                        builder[index] = '1';
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                builder.Remove(builder.Length - 1, 1);
+            }
+        }
+
+        return count;
+    }
+}
