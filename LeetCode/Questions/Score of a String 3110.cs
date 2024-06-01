53db0193a36a8685fb9622d9a9a4e2fd8ac8ba3a
s-alakayeu <allakaevs@gmail.com>
2024-06-01 12:22:51 +0300

    3110. Score of a String

---
+++ LeetCode/Questions/Score of a String 3110.cs
@@ -0,0 +1,11 @@
+﻿namespace LeetCode.Questions;
+
+public static class Score_of_a_String_3110
+{
+    public static int ScoreOfString(string s)
+    {
+        return s
+            .Zip(s.Skip(1), (a, b) => Math.Abs(a - b))
+            .Sum(); 
+    }
+}
