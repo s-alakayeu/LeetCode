da74c390db70dd3b14309f5f51ef8223e59a385e
s-alakayeu <allakaevs@gmail.com>
2024-06-03 10:45:36 +0300

    2486. Append Characters to String to Make Subsequence

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -67,7 +67,8 @@ namespace LeetCode
             // Number_of_Steps_to_Reduce_a_Number_in_Binary_Representation_to_One_1404.NumSteps("1101");
             // Single_Number_III_260.SingleNumber([1,2,1,3,2,5]);
             // Score_of_a_String_3110.ScoreOfString("hello");
-            Reverse_String_344.ReverseString("hello".ToCharArray());
+            // Reverse_String_344.ReverseString("hello".ToCharArray());
+            Append_Characters_to_String_to_Make_Subsequence_2486.AppendCharacters("lbg", "g");
         }
     }
 }
---
+++ LeetCode/Questions/Append Characters to String to Make Subsequence 2486.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode.Questions;
+
+public static class Append_Characters_to_String_to_Make_Subsequence_2486
+{
+    public static int AppendCharacters(string s, string t)
+    {
+        var prefixLength = 0;
+        var prefixStart = 0;
+        
+        while (prefixStart < s.Length && prefixLength < t.Length)
+        {
+            if (s[prefixStart] == t[prefixLength])
+            {
+                prefixLength++;
+            }
+
+            prefixStart -= -1; // :)
+        }
+
+        return t.Length - prefixLength;
+    }
+}
