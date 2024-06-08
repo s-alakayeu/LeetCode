dd9c46bd7481d2b86469dc7cf1945358f8399e48
s-alakayeu <allakaevs@gmail.com>
2024-06-08 11:17:31 +0300

    523. Continuous Subarray Sum

---
+++ LeetCode/Questions/Continuous Subarray Sum 523.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Questions;
+
+public static class Continuous_Subarray_Sum_523
+{
+    public static bool CheckSubarraySum(int[] nums, int k)
+    {
+        var prefixSum = nums[0] % k;
+
+        var prefixSumSet = new HashSet<int>();
+
+        for (var i = 1; i < nums.Length; i++)
+        {
+            var result = (prefixSum + nums[i]) % k;
+
+            if (result % k == 0 || prefixSumSet.Contains(result))
+            {
+                return true;
+            }
+
+            prefixSumSet.Add(prefixSum);
+
+            prefixSum = result;
+        }
+
+        return false;
+    }
+}
