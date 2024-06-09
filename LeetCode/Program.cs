e64d9688814ddc8e8ffbf47d621b6d9f2a7f6a3d
s-alakayeu <allakaevs@gmail.com>
2024-06-09 13:59:30 +0300

    974. Subarray Sums Divisible by K

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -79,7 +79,8 @@ namespace LeetCode
                     "xzao", "ug", "qnlvo", "fgbp", "dx", "m", "wp", "yoyf", "fyzl", "z", "mjcso", "s", "b", "u", "ad", "zrerd", "pyxxs", "g", "pgakz", "v", "owrpk", "slhb", "rprt"
                 ],
                 "zsyrupfxkwyzxnfkb nvsxmebaqwtejtdikjdb owabmqdbbo ltorldoq w xuyjdxmov hamjfoeae sqlftbmeywleeiscndj p jxu zoslotzcttjnbotki wyhyyueudixnorx vdglkopp cfakarjbf aboaszt onbffyfr cjpfrjqkolnamygqqj uwphkibxemxensz zouhcybstkbdyg ubfwglanyquhwlrglvc givqubayvmrvholkcke vkvyjrjraiollmlt npofa ffwnkttyp gwabsiibv vy tyrsvbuxticx e zfksmowe pdrlwocngdlpv n ouylngqjxilaqlkn zbxhafxxpvjkqpwdsy ssnsy itufigmudtvbv jw sfupomzekekabo caas nkmgrpuhskf rhifdognhfdoo pzecptqfzfvoa jzdlf zkcerlkxxqjy xzkwdgergjosa hgbovwtpzcpwtlwy tly ibtefvaozlsxo jopulkb mmgngozepqlpnqhvkn vwlbuktoipbncdnzhud urnocwb yfsrzjtvbt ybqbkflsz evlxlycwmlttaxlxdfjg xk tjjruytuikaucpcfxr ktbgnifciauv asvclppedfhf mldkna kbqtlbdwgftpzbojvsxm smzgn kyx zgclfw nsywjvgdicwccdoknm rhelltyrist fcijinwjev t");*/
-            Continuous_Subarray_Sum_523.CheckSubarraySum([23,2,6,4,7], 13);
+            // Continuous_Subarray_Sum_523.CheckSubarraySum([23,2,6,4,7], 13);
+            Subarray_Sums_Divisible_by_K_974.SubarraysDivByK([4, 5, 0, -2, -3, 1], 5);
         }
     }
 }
---
+++ LeetCode/Questions/Subarray Sums Divisible by K 974.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCode.Questions;
+
+public static class Subarray_Sums_Divisible_by_K_974
+{
+    public static int SubarraysDivByK(int[] nums, int k)
+    {
+        var count = 0;
+        var prefixSum = 0;
+        var prefixSumArrays = new int[k];
+        prefixSumArrays[0] = 1;
+
+        foreach (var num in nums)
+        {
+            prefixSum = ((prefixSum + num) % k + k) % k;
+
+            count += prefixSumArrays[prefixSum];
+            
+            prefixSumArrays[prefixSum]++;
+        }
+
+        return count;
+    }
+}
