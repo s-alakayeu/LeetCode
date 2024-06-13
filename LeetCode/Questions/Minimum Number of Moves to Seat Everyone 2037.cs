0d809122c745933d85e62329eb62355c3e314267
s-alakayeu <allakaevs@gmail.com>
2024-06-13 10:21:30 +0300

    2037. Minimum Number of Moves to Seat Everyone

---
+++ LeetCode/Questions/Minimum Number of Moves to Seat Everyone 2037.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Questions;
+
+public static class Minimum_Number_of_Moves_to_Seat_Everyone_2037
+{
+    public static int MinMovesToSeat(int[] seats, int[] students)
+    {
+        Array.Sort(seats);
+        Array.Sort(students);
+
+        var result = 0;
+
+        for (var i = 0; i < students.Length; i++)
+        {
+            result += Math.Abs(students[i] - seats[i]);
+        }
+
+        return result;
+    }
+}
