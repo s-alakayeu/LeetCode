7c225b2135d2008ed47aff11ddee49bb1b6cf1d4
s-alakayeu <allakaevs@gmail.com>
2024-06-15 20:21:47 +0300

    945. Minimum Increment to Make Array Unique

---
+++ LeetCode/Questions/Minimum Increment to Make Array Unique 945.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCode.Questions;
+
+public static class Minimum_Increment_to_Make_Array_Unique_945
+{
+    public static int MinIncrementForUnique(int[] nums)
+    {
+        Array.Sort(nums);
+
+        var moves = 0;
+        
+        for (var i = 1; i < nums.Length; i++)
+        {
+            if (nums[i] > nums[i - 1])
+            {
+                continue;
+            }
+            
+            var difference = nums[i - 1] - nums[i] + 1;
+            nums[i] += difference;
+            moves += difference;
+        }
+
+        return moves;
+    }
+}
