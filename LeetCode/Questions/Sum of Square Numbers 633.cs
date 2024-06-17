5af2925f94b1bd9cf84cc54b149987ee465831a2
s-alakayeu <allakaevs@gmail.com>
2024-06-17 19:11:45 +0400

    633. Sum of Square Numbers

--- LeetCode/Questions/Sum of Square Numbers 633.cs
+++ LeetCode/Questions/Sum of Square Numbers 633.cs
@@ -4,43 +4,28 @@ public static class Sum_of_Square_Numbers_633
 {
     public static bool JudgeSquareSum(int c)
     {
-        if (c == 0)
-        {
-            return true;
-        }
-        
-        for (var firstArgument = 0; firstArgument * firstArgument < c; firstArgument++)
-        {
-            var secondArgument = c - firstArgument * firstArgument;
+        var left = 0;
+        var right = (int)Math.Sqrt(c);
 
-            if (BinarySearch(0, secondArgument, secondArgument))
+        while (left <= right)
+        {
+            var sumOfSquares = left * left + right * right;
+
+            if (sumOfSquares == c)
             {
                 return true;
+            }
+
+            if (sumOfSquares < c)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
             }
         }
 
         return false;
-    }
-
-    private static bool BinarySearch(long start, long end, int secondArgument)
-    {
-        if (start > end)
-        {
-            return false;
-        }
-
-        long mid = start + (end - start) / 2;
-
-        if (mid * mid == secondArgument)
-        {
-            return true;
-        }
-
-        if (mid * mid > secondArgument)
-        {
-            return BinarySearch(start, mid - 1, secondArgument);
-        }
-        
-        return BinarySearch(mid + 1, end, secondArgument);
     }
 }
