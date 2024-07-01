49d0637d88ad9d430e542733c3d593eff77c5fdc
s-alakayeu <allakaevs@gmail.com>
2024-07-01 10:01:37 +0400

    1550. Three Consecutive Odds

---
+++ LeetCode/Questions/Three Consecutive Odds 1550.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetCode.Questions;
+
+public static class Three_Consecutive_Odds_1550
+{
+    public static bool ThreeConsecutiveOdds(int[] arr)
+    {
+        const int oddInRowReturnCondition = 3;
+        var oddInRow = 0;
+
+        for (var i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] % 2 == 0)
+            {
+                oddInRow = 0;
+                continue;
+            }
+
+            if (++oddInRow == oddInRowReturnCondition)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
